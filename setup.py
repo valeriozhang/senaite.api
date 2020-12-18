cb68438ea56e6a364edc966640e14c56fb54d716
valeriozhang <valeriozhang14@gmail.com>
2020-12-18 18:18:14 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.2.3-r2"
+version = "1.2.3rc7"
 
 with open("docs/About.rst", "r") as fh:
     long_description = fh.read()
@@ -22,9 +22,9 @@ with open("docs/Changelog.rst", "r") as fh:
     long_description += fh.read()
 
 setup(
-    name="senaite.api",
+    name="valer.api",
     version=version,
-    description="SENAITE API",
+    description="VALER API",
     long_description=long_description,
     # Get more strings from
     # http://pypi.python.org/pypi?:action=list_classifiers
@@ -34,9 +34,9 @@ setup(
         "Framework :: Zope2",
     ],
     keywords="",
-    author="SENAITE Foundation",
-    author_email="hello@senaite.com",
-    url="https://github.com/senaite/senaite.api",
+    author="Valer Group LLC",
+    author_email="valerio.zhang@valer.us",
+    url="https://github.com/valeriozhang/senaite.api",
     license="GPLv2",
     packages=find_packages("src", exclude=["ez_setup"]),
     package_dir={"": "src"},
@@ -46,7 +46,7 @@ setup(
     install_requires=[
         "setuptools",
         "plone.api",
-        "senaite.core",
+        "valer.core==1.3.5rc1",
     ],
     extras_require={
         "test": [

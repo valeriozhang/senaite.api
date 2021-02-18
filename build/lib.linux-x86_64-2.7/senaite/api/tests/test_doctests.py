e53f036a4b2e264691a3397b8f3aa4179454553c
Valerio Zhang <valeriozhang14@gmail.com>
2021-02-18 19:57:00 +0000

    Version Bump

---
+++ build/lib.linux-x86_64-2.7/senaite/api/tests/test_doctests.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2016-2017 BECHEM GmbH.
+
+import doctest
+
+import unittest2 as unittest
+
+from Testing import ZopeTestCase as ztc
+
+from .base import SimpleTestCase
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        ztc.ZopeDocFileSuite(
+            '../docs/API.rst',
+            '../docs/API_analysis.rst',
+            test_class=SimpleTestCase,
+            optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+        ),
+    ])
+    return suite
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = "1.2.3rc9"
+version = "1.2.3rc10"
 
 with open("docs/About.rst", "r") as fh:
     long_description = fh.read()
@@ -46,7 +46,7 @@ setup(
     install_requires=[
         "setuptools",
         "plone.api",
-        "valer.core==1.3.5rc8",
+        "valer.core==1.3.5rc9",
     ],
     extras_require={
         "test": [
